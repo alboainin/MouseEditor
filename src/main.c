35aa4d0876833823ca183aba66c2cc903945942e
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-04-09 14:28:54 +0300

    Fix unbuildable state.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-	struct app_state *state = malloc(sizeof(struct global_state));
+	struct app_state *state = malloc(sizeof(struct app_state));
 	state->argc = argc;
 	state->argv = argv;
 
