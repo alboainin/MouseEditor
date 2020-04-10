b9fd8a3008f5298b48eb15b0c08defe88d6eea38
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2020-04-10 13:27:21 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     
 
 	//printf("Hello, World!\nArgument count: %i", state->argc);
-    WriteFile(state);
-    ReadFile(state);
+    	WriteFile(state);
+    	ReadFile(state);
 
 }
