fa80c801995dc38126a69d9f5d076e2b8baecfbb
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-04-12 22:59:35 +0300

    Fix missing unistd.h on Windows Build.
    - Add win_unistd.h
    - Add platform check in file.h
    - Fix some missing comments.

--- src/file/file.h
+++ src/file/file.h
@@ -3,7 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#ifdef _WIN32
+#include "misc/win/win_unistd.h"
+#else
 #include <unistd.h>
+#endif
 
 #include "state.h"
 
@@ -11,4 +16,4 @@ int get_file_size(struct app_state *state);
 void write_file(struct app_state *state);
 void read_file(struct app_state *state);
 void log_file(FILE *filePtr, char ch);
-#endif
+#endif /* unistd.h */
